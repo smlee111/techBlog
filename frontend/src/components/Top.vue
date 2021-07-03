ae9cbce55579a381521e851e25c1f5f431a63d48
smlee111 <vic_sm@naver.com>
2021-07-03 23:33:09 +0900

    스크롤 감지 테스트

--- frontend/src/components/Top.vue
+++ frontend/src/components/Top.vue
@@ -3,12 +3,29 @@
         <div class="w1160">
             <div class="titWrap">
                 <p class="tit">Blog</p>
-                <p class="sub">개발자 이승민 기술 블로그 방문을 환영합니다.</p>
+                <p class="sub">개발자 이승민 기술 블로그 방문을 환영합니다11.</p>
                 <p class="sub">좋은 개발자로 거듭나는 그 날까지! 기술과 노하우를 공유해보아요 :)</p>
             </div>
         </div>
     </div>
 </template>
+
+<script>
+export default {
+    created() {
+        window.addEventListener('scroll', this.handleScroll);
+    },
+    destroyed() {
+        window.removeEventListener('scroll', this.handleScroll);
+    },
+    methods: {
+        handleScroll (event) {
+            alert("스크롤 감지");
+        }
+    }
+}
+</script>
+
 
 <style scoped>
     .top {background-image:url(../assets/images/intro_blog.png);}
